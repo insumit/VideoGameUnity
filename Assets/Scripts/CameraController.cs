c2afab8063057a07211b8fb729bef4eb5363d5f1
Sumit Mandal <sumit.m@holosuit.com>
2020-11-27 17:04:45 +0530

    init commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject gameCamera, targetObject;
+
+    public bool enableTranslation = false;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (enableTranslation)
+        {
+            TranslateCamera();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        enableTranslation = true;
+    }
+
+    void TranslateCamera()
+    {
+        gameCamera.transform.Translate((targetObject.transform.position - gameCamera.transform.position).normalized * Time.deltaTime * 3f, Space.World);
+        gameCamera.transform.rotation = Quaternion.Slerp(gameCamera.transform.rotation, targetObject.transform.rotation, Time.deltaTime * 3f);
+    }
+}
